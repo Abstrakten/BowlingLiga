b85a99f811581c041e178e6fb819839e6a08c4b0
Abstrakten <kterndrupxd@gmail.com>
2015-07-19 22:57:33 +0200

    button stuff

--- BowlingLiga/app/src/main/java/svin/bowlingliga/RegisterScoreActiviten.java
+++ BowlingLiga/app/src/main/java/svin/bowlingliga/RegisterScoreActiviten.java
@@ -1,34 +1,23 @@
 package svin.bowlingliga;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class LigaListen extends AppCompatActivity {
+public class RegisterScoreActiviten extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_liga_listen);
-        Button yourButton = (Button)findViewById(R.id.RegButton);
-        yourButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LigaListen.this, RegisterScoreActiviten.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_register_score_activiten);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_liga_listen, menu);
+        getMenuInflater().inflate(R.menu.menu_register_score_activiten, menu);
         return true;
     }
 
@@ -47,4 +36,3 @@ public class LigaListen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
