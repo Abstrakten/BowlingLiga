d9b9bd49813ea8a3ded870dee11d429f02b99a87
Morten Terndrup <morten@mokote.dk>
2015-07-22 01:01:09 +0200

    webservice til submission af games. Mangler unit tests.

--- web/resources/api/submitGame.php
+++ web/resources/api/submitGame.php
@@ -1,29 +1,126 @@
 <?php
+// Debug
+ob_start();
 require_once '../dbconfig.php';
-require_once 'functions/security.php';
-// Create connection
+require_once '../library/security.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Authenticate user
+// User credentials
 $name = mysqli_escape_string($conn, $_POST["username"]);
-$pass = mysqli_escape_string($conn, hash("sha256", $salt1.$_POST["password"].$salt2));
-$email = mysqli_escape_string($conn, $_POST["email"]);
-$sql = "INSERT INTO users (username, password, email)
-VALUES ('$name', '$pass', '$email')";
+$pass = mysqli_escape_string($conn, $_POST["password"]);
+// Debug
+//var_dump($name);
+//var_dump($pass);
 
-// 
+// Game details
+$score1 = $_POST["score1"];       // score1
+$score2 = $_POST["score2"];       // score2
+if($_POST["hasTeams"] == 'TRUE'){
+    // Debug
+    // echo "has teams </br>";
+    $team1 = mysqli_escape_string($conn, $_POST["team1"]);
+    $team2 = mysqli_escape_string($conn, $_POST["team2"]);
+    if (authUser($conn, $name, $pass) === FALSE) {
+        die("Username and password combination invalid.</br>");
+    }
+    else{
+        // Debug
+        //echo "Authenticated successfully... </br>";
+        // get ids of the teams TODO: use team names asa identifier, since they are unique ?
+        $teamId1 = $conn->query("SELECT id FROM teams WHERE name = '$team1'");
+        $teamId2 = $conn->query("SELECT id FROM teams WHERE name = '$team2'");
+        $team1 = $teamId1->fetch_assoc();
+        $team2 = $teamId2->fetch_assoc();
 
+        $team1 = $team1["id"];
+        $team2 = $team2["id"];
+        // debug
+        //echo "Team 1 id is: " . $team1 . " </br>";
+        //echo "Team 2 id is: " . $team2 . "</br>";
+        if(empty($team1) || empty($team2)){
+            die("One of the teams does not exist.</br>");
+        }
+        $sql = "INSERT INTO games (team1, team2, score1, score2)
+                VALUES ('$team1', '$team2', '$score1', '$score2')";
 
-
-if ($conn->query($sql) === TRUE) {
-    echo "Success!";
-
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+        if ($conn->query($sql) === TRUE) {
+            echo "Success!";
+        } else {
+            echo $conn->error;
+        }
+        $conn->close();
+    }
 }
+else{
+    // Debug
+    // echo "has NO teams... </br>";
+    if (authUser($conn, $name, $pass) === FALSE){
+        die("Username and password combination invalid.</br>");
+    }
+    else{
+        //echo 'Authentication successfull!';
+        echo "Authenticated successfully</br>";
+        $player1 = $_POST["player1"];     // team1
+        $player2 = $_POST["player2"];     // team1
+        $player3 = $_POST["player3"];     // team2
+        $player4 = $_POST["player4"];     // team2
 
-$conn->close();
+        // Get ids of players
+        $player1 = $conn->query("SELECT id FROM players WHERE username = '$player1' ")->fetch_assoc()["id"];
+        $player2 = $conn->query("SELECT id FROM players WHERE username = '$player2' ")->fetch_assoc()["id"];
+        $player3 = $conn->query("SELECT id FROM players WHERE username = '$player3' ")->fetch_assoc()["id"];
+        $player4 = $conn->query("SELECT id FROM players WHERE username = '$player4' ")->fetch_assoc()["id"];
+
+        // Debug
+        //var_dump("player1 is " . $player1) ."\n";
+        //var_dump("player2 is " . $player2). "\n";
+        //var_dump("player3 is " . $player3). "\n";
+        //var_dump("player4 is " . $player4). "\n";
+
+        if ($player1 === FALSE || $player2 === FALSE || $player3 === FALSE || $player4 === FALSE){
+            die("one of the players does not exist.</br>");
+        }
+        // Try to find existing teams based on player ids
+        $teamId1 = $conn->query("SELECT id
+                    FROM teams
+                    WHERE (player1 = '$player1' AND player2 = '$player2')
+                    OR (player1 = '$player2' AND player2 = '$player1')");
+        if(teamId1 === FALSE){
+            die("No existing teams exist for player with id " . $player1 . " and " . $player2);
+        }
+        else {
+            $teamId1 = $teamId1->fetch_assoc()["id"];
+        }
+        $teamId2 = $conn->query("SELECT id
+                    FROM teams
+                    WHERE (player1 = '$player3' AND player2 = '$player4')
+                    OR (player1 = '$player4' AND player2 = '$player3')");
+        if(teamId2 === FALSE){
+            die("No existing teams exist for player with id " . $player3 . " and " . $player4);
+        }
+        else {
+            $teamId2 = $teamId2->fetch_assoc()["id"];
+        }
+        // Debug
+        //var_dump("teamId1 is ". $teamId1) ;
+        //var_dump("teamId2 is ". $teamId2) ;
+
+        // Prepare submission of new game
+        date_default_timezone_set('Europe/Copenhagen');
+        // Old
+        //$time = date('m/d/Y h:i:s a', time());
+        // Convert to sql format
+        $time = date('Y-m-d H:i:s', time());
+        $sql = "INSERT INTO games (team1, team2, score1, score2, played_on)
+                VALUES ('$teamId1', '$teamId2', '$score1', '$score2', '$time')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Success!";
+        } else {
+            echo $conn->error;
+        }
+        $conn->close();
+    }
+}
 ?>
--- web/test.php
+++ web/test.php
@@ -1,7 +1,195 @@
-<form action="resources/api/registerUser.php" method="POST">
-Navn: <input type="text" name="username">
-<br>
-Kodeord: <input type="text" name="password">
-<br>
-<input type="submit" value="Test">
-</form>
+<?php
+ob_start();
+function runSubmitGameUnitTests(){
+    testSubmitGamesInvalidUsername();
+    testSubmitGamesInvalidPassword();
+    testSubmitGamesWithTeamsTeam1Bad();
+    testSubmitGamesWithTeamsTeam2Bad();
+    testSubmitGamesWithTeamsGood();
+    testSubmitGamesNoTeamsBad();
+}
+function testSubmitGamesInvalidUsername(){
+    echo "Testing for invalid username...</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'a',
+        'password' => 'b',
+        'team1' => 'svin',
+        'team2' => 'nerdboosters',
+        'score1' => '5',
+        'score2' => '5',
+        'hasTeams' => 'TRUE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result . "</br>";
+}
+function testSubmitGamesInvalidPassword(){
+    echo "Testing for VALID username with invalid password...</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => '1234',
+        'team1' => 'svin',
+        'team2' => 'nerdboosters',
+        'score1' => '5',
+        'score2' => '5',
+        'hasTeams' => 'TRUE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result . "</br>";
+}
+function testSubmitGamesWithTeamsGood(){
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => 'test2',
+        'team1' => 'svin',
+        'team2' => 'nerdboosters',
+        'score1' => '5',
+        'score2' => '5',
+        'hasTeams' => 'TRUE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    var_dump($result);
+}
+function testSubmitGamesWithTeamsTeam1Bad(){
+    echo "Testing for invalid team 1...</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => 'test2',
+        'team1' => 'wrongteam1',
+        'team2' => 'nerdboosters',
+        'score1' => '5',
+        'score2' => '5',
+        'hasTeams' => 'TRUE',
+    );
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    echo $result . "</br>";
+}
+function testSubmitGamesWithTeamsTeam2Bad(){
+    echo "Testing for invalid team 2...</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => 'test2',
+        'team1' => 'nerdboosters',
+        'team2' => 'wrongapp',
+        'score1' => '5',
+        'score2' => '5',
+        'hasTeams' => 'TRUE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result . "</br>";
+}
+function testSubmitGamesNoTeamsGood(){
+    echo "Testing No teams good</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => 'test2',
+        'player1' => 'svin',
+        'player2' => 'fedesvin',
+        'player3' => 'Hotdogfun',
+        'player4' => 'four',
+        'score1' => '1234',
+        'score2' => '5678',
+        'hasTeams' => 'FALSE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result . "</br>";
+}
+function testSubmitGamesNoTeamsBad(){
+    echo "Testing No teams bad</br>";
+    $url = 'http://beer.mokote.dk/resources/api/submitGame.php';
+    $data = array(
+        'username' => 'test2',
+        'password' => 'test2',
+        'player1' => 'svin',
+        'player2' => 'fedesvin',
+        'player3' => 'Hotdogfun',
+        'player4' => 'four',
+        'score1' => '1234',
+        'score2' => '5678',
+        'hasTeams' => 'FALSE',
+    );
+
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    echo $result . "</br>";
+}
+runSubmitGameUnitTests();
+?>
