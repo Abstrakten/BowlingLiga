c9db00bfb172482549d6a06ec88ea3a0a0c1ca72
Morten <motn92@gmail.com>
2016-03-30 15:52:50 +0200

    removed backend to seperate repository

--- web/login.php
+++
@@ -1,6 +0,0 @@
-<form class="fg" action="functions/auth.php" method="POST">
-Brugernavn: <input class="resizedTextbox" type="text" name="username">
-Kodeord: <input class="resizedTextbox" type="password" name="password">
-<input type="submit" value="Log ind">
-</form>
-<a href="register.php">Registrér</a>
